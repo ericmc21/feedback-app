7277c312640f2d2f3b5582a68303a0d95841d475
Eric McLean <eric.mclean@infermedica.com>
2023-06-11 09:22:57 -0400

    completed section 3.7 FeedbackStats Component

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function FeedbackStats() {
-  return <div>Stats</div>;
+export default function FeedbackStats({ feedback }) {
+  // calculate ratings aversage
+  let average =
+    feedback.reduce((accumulator, current) => {
+      return accumulator + current.rating;
+    }, 0) / feedback.length;
+
+  average = average.toFixed(1).replace(/[.,]0$/, "");
+
+  return (
+    <div className="feedack-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Averate Rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
+  );
 }
+
+FeedbackStats.propTypes = {
+  feedback: PropTypes.array.isRequired,
+};
