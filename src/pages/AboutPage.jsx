0693f768d8ad44c6c9d00b9b6826b0e99cfd9966
Eric McLean <eric.mclean@infermedica.com>
2023-08-06 15:26:57 -0400

    commented out card in about

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Card from "../components/shared/Card";
+//import Card from "../components/shared/Card";
 
 export default function AboutPage() {
   return (
